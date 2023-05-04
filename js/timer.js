46a19c1592d3fa136f1a0ff823ee6866f6a8c29d
Vladislav Zhukov <vlad.zhukov24@gmail.com>
2023-05-04 18:05:12 +0300

    some fixes

---
+++ js/timer.js
@@ -0,0 +1,28 @@
+// Установите дату, до которой мы ведем обратный отсчет
+var countDownDate = new Date("May 11, 2023 09:00:00").getTime();
+
+// Обновляйте обратный отсчет каждые 1 секунду
+var x = setInterval(function() {
+
+  // Получить сегодняшнюю дату и время
+  var now = new Date().getTime();
+    
+  // Найдите расстояние между моментом сейчас и датой обратного отсчета
+  var distance = countDownDate - now;
+    
+  // Расчеты времени для дней, часов, минут и секунд
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Выведите результат в виде элемента с id="demo"
+  document.getElementById("demo").innerHTML = days + "д " + hours + "ч "
+  + minutes + "м " + seconds + "с ";
+    
+  // Если обратный отсчет закончился, напишите какой-нибудь текст
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
